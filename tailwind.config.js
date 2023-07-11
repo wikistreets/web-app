4a3e7e4d000f44118a5cb03adbedecd8e424b2b4
rubykiim <sk7383@nyu.edu>
2023-07-11 10:39:09 -0400

    set textColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,18 @@ module.exports = {
       },
     },
     extend: {
+      textColor: {
+        primary: "#1F2937", // gray-800
+        secondary: "#6B7280", // gray-500
+        tertiary: "#E5E7EB" // gray-200
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
