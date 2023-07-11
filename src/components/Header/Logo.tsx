db72e4134d74706b45d8e985b873c1a3efa6dd86
rubykiim <sk7383@nyu.edu>
2023-07-11 15:49:27 -0400

    Create Logo.tsx

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Logo() {
+  return (
+    <div className="flex justify-center items-center w-9 h-9 border border-slate-300 text-2xl">
+      W
+    </div>
+  );
+}
