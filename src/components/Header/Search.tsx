0e357862a674ddce13f4a6539a25d1c46f97733a
rubykiim <sk7383@nyu.edu>
2023-07-11 15:49:32 -0400

    Create Search.tsx

---
+++ src/components/Header/Search.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+
+export default function Search() {
+  const handleSearch = () => {};
+
+  return (
+    <div
+      onClick={handleSearch}
+      className="flex w-full items-center justify-center px-4 sm:justify-start"
+    >
+      <FontAwesomeIcon
+        icon={icon({ name: "magnifying-glass", style: "solid" })}
+        style={{ color: "#9CA3AF" }}
+        className="mr-2"
+      />
+      <h2 className="text-secondary">Search</h2>
+    </div>
+  );
+}
