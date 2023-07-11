b22bc22045674707d982245815422986c2e8ecec
rubykiim <sk7383@nyu.edu>
2023-07-11 19:00:05 -0400

    Create UserPic.tsx

---
+++ src/components/Profile/UserPic.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import userDefault from "../../../public/icons/user-default.svg";
+
+export default function UserPic() {
+  const [userId, setUserId] = useState(null);
+  const [userPic, setUserPic] = useState(userDefault);
+
+  return (
+    <div className="flex shrink-0 grow w-9 h-9">
+      <Image src={userPic} alt="User"></Image>
+    </div>
+  );
+}
