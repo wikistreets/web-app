db94698fe4cf923d9f5efcc9bf3a3fe8fad80219
rubykiim <sk7383@nyu.edu>
2023-07-12 15:00:12 -0400

    'View more' onClick setup

--- src/components/Landing/FeaturedMaps/FeaturedMaps.tsx
+++ src/components/Landing/FeaturedMaps/FeaturedMaps.tsx
@@ -9,26 +9,32 @@ interface MapData {
 }
 
 const FeaturedMaps: React.FC = () => {
-  const mapData: MapData[] = [
-    { img: "img1", title: "Washington D.C. Family Trip" },
-    { img: "img2", title: "Anatomy of Vermeer's Milkmaid" },
-    { img: "img3", title: "Damaged Sidewalks in Croton" },
-    { img: "img4", title: "School Walking Routes" },
-  ];
+    const mapData: MapData[] = [
+      { img: "img1", title: "Washington D.C. Family Trip" },
+      { img: "img2", title: "Anatomy of Vermeer's Milkmaid" },
+      { img: "img3", title: "Damaged Sidewalks in Croton" },
+      { img: "img4", title: "School Walking Routes" },
+    ];
 
-  return (
-    <div className="bg-gradient-to-b from-indigo-600 to-white w-full py-8 flex flex-col gap-6 justify-center items-center">
-      <h2 className="font-dm-sans text-white tracking-wide">Featured Maps</h2>
+    const handleViewMore = () => {
+      console.log("View More clicked!");
+    };
 
-      <div className="grid grid-cols-1 grid-rows-4 gap-3 justify-items-center items-center w-full h-fit px-2.5">
-        {mapData.map((data, idx) => (
-          <MapCard key={idx} img={data.img} title={data.title} />
-        ))}
+    return (
+      <div className="bg-gradient-to-b from-indigo-600 to-white w-full py-8 flex flex-col gap-6 justify-center items-center">
+        <h2 className="font-dm-sans text-white tracking-wide">Featured Maps</h2>
+
+        <div className="grid grid-cols-1 grid-rows-4 gap-3 justify-items-center items-center w-full h-fit px-2.5">
+          {mapData.map((data, idx) => (
+            <MapCard key={idx} img={data.img} title={data.title} />
+          ))}
+        </div>
+
+        <Button onClick={handleViewMore} className="w-fit text-white">
+          View more
+        </Button>
       </div>
-
-      <Button className="w-fit text-white">View more</Button>
-    </div>
-  );
+    );
 };
 
 export default FeaturedMaps;
