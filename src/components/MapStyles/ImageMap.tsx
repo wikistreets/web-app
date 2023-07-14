a86dfb8e02f555fd5ef41830d8f96c9672ead6d3
rubykiim <sk7383@nyu.edu>
2023-07-14 13:58:01 -0400

    Create ImageMap.tsx

---
+++ src/components/MapStyles/ImageMap.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import imageMap from "../../../public/media/image-map.png";
+
+type Props = {};
+
+const ImageMap = (props: Props) => {
+  return (
+    <section
+      className="flex flex-col sm:flex-row sm:grid-cols-3 
+      justify-center sm:items-center
+      gap-3 sm:gap-12 lg:gap-16 xl:gap-20
+      max-w-sm sm:min-w-full"
+    >
+      <Image
+        src={imageMap}
+        alt="Custom Image Map"
+        className="rounded-lg object-cover sm:max-w-sm sm:h-max sm:col-span-1"
+      />
+
+      <section
+        className="flex flex-col justify-center gap-3 lg:gap-7 xl:gap-9 2xl:gap-12
+        sm:col-span-2 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl"
+      >
+        <h3
+          className="font-dm-sans font-semibold tracking-wide
+          text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl"
+        >
+          Custom Image Map
+        </h3>
+        <p
+          className="text-secondary font-light lg:leading-relaxed
+          text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg"
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+          consectetur ipsum, voluptatum harum, quos quidem pariatur facere porro
+          ullam quae ut dolorem corrupti deserunt illum autem explicabo
+          veritatis culpa consequatur!
+        </p>
+
+        {/* TODO: make it a component if keeping... */}
+        <section
+          className="flex justify-between 
+          sm:justify-start sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24 2xl:gap-28"
+        >
+          <div className="flex flex-col">
+            <h3 className="font-bold text-indigo-600 text-lg sm:text-md md:text-lg lg:text-xl xl:text-2xl">
+              24/7
+            </h3>
+            <p className="text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg">
+              Delivery
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-bold text-indigo-600 text-lg sm:text-md md:text-lg lg:text-xl xl:text-2xl">
+              99.9%
+            </h3>
+            <p className="text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg">
+              Accuracy
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-bold text-indigo-600 text-lg sm:text-md md:text-lg lg:text-xl xl:text-2xl">
+              100k+
+            </h3>
+            <p className="text-secondary text-sm sm:text-xs md:text-sm lg:text-md xl:text-lg">
+              Flexibility
+            </p>
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default ImageMap;
