bc93bf0e3bac93b4a6a853f7b065bf1033f7965b
rubykiim <sk7383@nyu.edu>
2023-07-20 16:31:00 -0400

    integrate React Leaflet

--- src/components/FeaturedMaps/MapCard.tsx
+++ src/components/FeaturedMaps/MapCard.tsx
@@ -1,25 +1,46 @@
 "use client";
 
-import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 interface MapCardProps {
-  img: string;
+  centerX: number;
+  centerY: number;
+  markerX: number;
+  markerY: number;
   title: string;
 }
 
-const MapCard: React.FC<MapCardProps> = ({ img, title }) => {
+const MapCard: React.FC<MapCardProps> = ({
+  centerX,
+  centerY,
+  markerX,
+  markerY,
+  title,
+}) => {
+  const mapContainerId = uuidv4();
+
   return (
     <figure
       className="flex flex-col justify-center items-center w-full px-4 pt-4 rounded-lg bg-white
       max-w-sm 2xl:max-w-md"
     >
-      <Image
-        src={`/${img}`}
-        width={338}
-        height={192}
-        alt={title}
+      <MapContainer
+        id={mapContainerId}
         className="w-full h-48 text-center"
-      />
+        center={[centerX, centerY]}
+        zoom={13}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        attributionControl={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[markerX, markerY]}>
+          {/* <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup> */}
+        </Marker>
+      </MapContainer>
       <figcaption className="w-full py-4 text-center text-md">
         {title}
       </figcaption>
