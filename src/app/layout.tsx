979cdb3669264618821b5b8e3841387462cc0d33
rubykiim <sk7383@nyu.edu>
2023-07-20 11:51:22 -0400

    React Leaflet set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${dm_sans.variable}`}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+        <script
+          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossOrigin=""
+        ></script>
+      </head>
       <body className="flex flex-col min-h-screen">
         <div className="container mx-auto">
           <Header />
