14f9f94df3e806196350d951b34ccc180be0b509
rubykiim <sk7383@nyu.edu>
2023-07-21 10:37:22 -0400

    Create Nav.tsx

---
+++ src/components/Feed/Post/Header/Nav.tsx
@@ -0,0 +1,45 @@
+"use cleint";
+
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+type Props = {};
+
+const Nav = (props: Props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isMyPost, setIsMyPost] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-fit h-9 flex justify-center items-center">
+          <FontAwesomeIcon icon={faEllipsisVertical} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mr-2.5">
+          {/* TODO: clarify logic */}
+          {!isLoggedIn && (
+            <>
+              <DropdownMenuItem>Share</DropdownMenuItem>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <DropdownMenuItem>Share</DropdownMenuItem>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default Nav;
