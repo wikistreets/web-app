dbbbc4077947633841adb8654f0a09cff31ec2a4
rubykiim <sk7383@nyu.edu>
2023-07-22 14:07:49 -0400

    Create Bar.tsx

---
+++ src/components/Feed/Post/Bar.tsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faFileLines } from "@fortawesome/free-solid-svg-icons";
+
+type BarProps = {
+  location?: string;
+  postTitle?: string;
+};
+
+const Bar = (props: BarProps) => {
+  const geoMapBar = (
+    <>
+      <FontAwesomeIcon
+        icon={faLocationDot}
+        size="lg"
+        className="text-indigo-600"
+      />
+      <h6 className="text-indigo-600 text-md font-medium">{props.location}</h6>
+    </>
+  );
+  const imageMapBar = (
+    <>
+      <FontAwesomeIcon
+        icon={faFileLines}
+        size="lg"
+        className="text-indigo-600"
+      />
+      <h6 className="text-indigo-600 text-md font-medium">{props.postTitle}</h6>
+    </>
+  );
+  return (
+    <section className="bg-indigo-50 flex flex-row gap-2 px-2 py-3 justify-start items-center">
+      {props.location && geoMapBar}
+      {props.postTitle && imageMapBar}
+    </section>
+  );
+};
+
+export default Bar;
