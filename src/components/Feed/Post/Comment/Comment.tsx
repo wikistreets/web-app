93f470c09edd8842402bbd884db7a5c0ba926887
rubykiim <sk7383@nyu.edu>
2023-07-22 14:28:02 -0400

    Create Comment.tsx

---
+++ src/components/Feed/Post/Comment/Comment.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+const Comment = (props: Props) => {
+  return (
+    <div className="flex flex-row gap-1 justify-start items-center">
+      <p className="text-primary font-medium">User</p>
+      <p>Gotta check this place out!</p>
+    </div>
+  );
+};
+
+export default Comment;
