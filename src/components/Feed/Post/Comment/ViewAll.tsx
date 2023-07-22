442a6059fc78584182092cf97979779ad6eef8ac
rubykiim <sk7383@nyu.edu>
2023-07-22 14:28:17 -0400

    Create ViewAll.tsx

---
+++ src/components/Feed/Post/Comment/ViewAll.tsx
@@ -0,0 +1,13 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type Props = {};
+
+const ViewAll = (props: Props) => {
+  return <div className="w-full">View all comments</div>;
+};
+
+export default ViewAll;
