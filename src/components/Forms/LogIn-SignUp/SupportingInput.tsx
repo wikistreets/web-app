e4d4e9d0a1bfd9d89ab7a0830a0dfa141d33194a
rubykiim <sk7383@nyu.edu>
2023-07-26 12:53:54 -0500

    Create SupportingInput.tsx

---
+++ src/components/Forms/LogIn-SignUp/SupportingInput.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const SupportingInput = () => {
+  return (
+    <div className="flex flex-row justify-between items-center">
+      <div className="flex flex-row justify-start items-center gap-2">
+        <Checkbox className="border-gray-200" />
+        <label
+          htmlFor="rememberMe"
+          className="text-xs text-secondary leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Remember me
+        </label>
+      </div>
+      <Link href="/" className="text-indigo-600 text-xs">
+        Forgot password?
+      </Link>
+    </div>
+  );
+};
+
+export default SupportingInput;
+// hello! kiki was here
