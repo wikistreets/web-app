57540ba36d61711eaed9732b4d77e7e3718a7845
rubykiim <sk7383@nyu.edu>
2023-07-26 12:53:24 -0500

    add form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,17 @@
-export default function page() {
-  return <div>Login!</div>;
-}
+import LogInForm from "@/components/Forms/LogIn-SignUp/LogInForm";
+
+type Props = {};
+
+const LogIn = (props: Props) => {
+  return (
+    <section
+      className="flex flex-col h-full justify-center gap-8
+      px-4 sm:px-6 md:px-10 lg:px-14 xl:px-16"
+    >
+      <h1 className="font-dm-sans tracking-wide">Welcome back!</h1>
+      <LogInForm />
+    </section>
+  );
+};
+
+export default LogIn;
