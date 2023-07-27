2962ff099eed1ab8ab4cc000aae035b1aab6afa8
rubykiim <sk7383@nyu.edu>
2023-07-27 13:42:01 -0500

    switch to google icon with color

--- src/components/Forms/LogIn-SignUp/SocialLogIn.tsx
+++ src/components/Forms/LogIn-SignUp/SocialLogIn.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import { Button } from "../../ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FcGoogle } from "react-icons/fc";
 
 type SocialLogInProps = {};
 
 const SocialLogIn = (props: SocialLogInProps) => {
   return (
     <div className="flex flex-row gap-2">
-      <Button className="bg-white border border-solid border-slate-200 rounded-lg w-1/2 h-min">
+      <Button
+        className="bg-white border border-solid border-slate-200 rounded-lg w-1/2 h-min
+      flex"
+      >
         <Link href="/" className="w-full">
-          <FontAwesomeIcon icon={faGoogle} />{" "}
+          <FcGoogle className="mx-auto" size="1.2rem" />
         </Link>
       </Button>
 
