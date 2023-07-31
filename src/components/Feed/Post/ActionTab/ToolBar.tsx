1225808b7d13f7167958bef41fbee52dd1d70c2c
rubykiim <sk7383@nyu.edu>
2023-07-31 16:32:45 -0500

    use menubar ui & add admin setting btn

--- src/components/Feed/Post/ActionTab/ToolBar.tsx
+++ src/components/Feed/Post/ActionTab/ToolBar.tsx
@@ -1,21 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+  MenubarSub,
+  MenubarSubTrigger,
+  MenubarSubContent,
+} from "@/components/ui/menubar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faArrowUpFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
-import { PiPlusCircleBold } from "react-icons/pi";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { PiPlusCircleBold, PiGearBold } from "react-icons/pi";
 
 type ToolBarProps = {};
 
-export const ToolBar: React.FC<ToolBarProps> = (props) => {
+export const ToolBar: React.FC<ToolBarProps> = () => {
+  const [isAdmin, setIsAdmin] = useState(true);
+
   const handleSearch = () => {
     console.log("search");
   };
@@ -25,13 +34,120 @@ export const ToolBar: React.FC<ToolBarProps> = (props) => {
   const handleCreate = () => {
     console.log("Create map? post?");
   };
+  const handleSettings = () => {};
 
   return (
-    <div className="flex gap-6 justify-center items-center bg-white w-full text-indigo-600">
-      <FontAwesomeIcon icon={faMagnifyingGlass} onClick={handleSearch} />
-      <FontAwesomeIcon icon={faArrowUpFromBracket} onClick={handleShare} />
-      <PiPlusCircleBold size="1.3rem" onClick={handleCreate} />
-    </div>
+    <Menubar className="flex gap-6 justify-center items-center bg-white w-full text-indigo-600 border-none">
+      {/* SEARCH */}
+      <MenubarMenu>
+        <MenubarTrigger>
+          <>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              onClick={handleSearch}
+              className="lg:hidden"
+            />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              onClick={handleSearch}
+              size="lg"
+              className="hidden lg:block"
+            />
+          </>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Search</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+
+      {/* SHARE */}
+      <MenubarMenu>
+        <MenubarTrigger>
+          <>
+            <FontAwesomeIcon
+              icon={faArrowUpFromBracket}
+              onClick={handleShare}
+              className="lg:hidden"
+            />
+            <FontAwesomeIcon
+              icon={faArrowUpFromBracket}
+              onClick={handleShare}
+              size="lg"
+              className="hidden lg:block"
+            />
+          </>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarSub>
+            <MenubarSubTrigger>Share</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Instagram</MenubarItem>
+              <MenubarItem>Threads</MenubarItem>
+              <MenubarItem>Twitter</MenubarItem>
+              <MenubarItem>Messages</MenubarItem>
+              <MenubarItem>Email</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>
+            Copy link
+            {/* <MenubarShortcut>⌘T</MenubarShortcut> */}
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+
+      {/* CREATE */}
+      <MenubarMenu>
+        <MenubarTrigger>
+          <>
+            <PiPlusCircleBold
+              size="1.1rem"
+              onClick={handleCreate}
+              className="lg:hidden"
+            />
+            <PiPlusCircleBold
+              size="1.3rem"
+              onClick={handleCreate}
+              className="hidden lg:block"
+            />
+          </>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Map
+            {/* <MenubarShortcut>⌘M</MenubarShortcut> */}
+          </MenubarItem>
+          <MenubarItem>New Post</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+
+      {/* ADMIN FEATURES */}
+      {isAdmin && (
+        <>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <>
+                <PiGearBold
+                  size="1.1rem"
+                  onClick={handleCreate}
+                  className="lg:hidden"
+                />
+                <PiGearBold
+                  size="1.3rem"
+                  onClick={handleCreate}
+                  className="hidden lg:block"
+                />
+              </>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>Map settings</MenubarItem>
+              <MenubarItem>Reorder posts</MenubarItem>
+              <MenubarItem>Delete map</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </>
+      )}
+    </Menubar>
   );
 };
 
