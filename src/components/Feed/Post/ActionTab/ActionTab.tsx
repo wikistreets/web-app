cd4dce8c739f675aa6a8ab6c005c03debbb22e0d
rubykiim <sk7383@nyu.edu>
2023-07-31 16:32:52 -0500

    update gap

--- src/components/Feed/Post/ActionTab/ActionTab.tsx
+++ src/components/Feed/Post/ActionTab/ActionTab.tsx
@@ -7,8 +7,9 @@ type ActionTabProps = {};
 export const ActionTab: React.FC<ActionTabProps> = (props) => {
   return (
     <div
-      className="flex flex-col w-full justify-center items-center bg-white gap-4 pb-4
-      sticky top-60 sm:top-0 z-20"
+      className="flex flex-col w-full justify-center items-center pb-4 bg-white
+      gap-1 md:gap-2
+      z-20 sticky top-60 sm:top-0"
     >
       <Dropdown />
       <ToolBar />
