05f933d7eff154b7952fad66a41f0543f087a4cf
rubykiim <sk7383@nyu.edu>
2023-07-31 10:29:09 -0500

    Create ActionTab.tsx

---
+++ src/components/Feed/Post/ActionTab/ActionTab.tsx
@@ -0,0 +1,20 @@
+import Dropdown from "./Dropdown";
+import ToolBar from "./ToolBar";
+import Info from "./Info";
+
+type ActionTabProps = {};
+
+export const ActionTab: React.FC<ActionTabProps> = (props) => {
+  return (
+    <div
+      className="flex flex-col w-full justify-center items-center bg-white gap-4 pb-4
+      sticky top-60 sm:top-0"
+    >
+      <Dropdown />
+      <ToolBar />
+      <Info totalPosts={30} totalContributors={9} totalCopies={25} />
+    </div>
+  );
+};
+
+export default ActionTab;
