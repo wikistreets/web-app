c2283eb66709d926353c914af5ccb0f1dfebd780
rubykiim <sk7383@nyu.edu>
2023-07-31 15:27:46 -0500

    add z-index

--- src/components/Feed/Post/ActionTab/ActionTab.tsx
+++ src/components/Feed/Post/ActionTab/ActionTab.tsx
@@ -8,7 +8,7 @@ export const ActionTab: React.FC<ActionTabProps> = (props) => {
   return (
     <div
       className="flex flex-col w-full justify-center items-center bg-white gap-4 pb-4
-      sticky top-60 sm:top-0"
+      sticky top-60 sm:top-0 z-20"
     >
       <Dropdown />
       <ToolBar />
