1762eb1c95661fb477e71a1781a326514b0a2d6a
rubykiim <sk7383@nyu.edu>
2023-07-31 10:29:53 -0500

    Create Map.tsx

---
+++ src/components/Feed/Post/Map.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+type Props = {};
+
+const MapInPost = (props: Props) => {
+  return (
+    <>
+      <MapContainer
+        className="w-full h-60 sticky top-0 z-20
+        sm:aspect-square sm:h-full"
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        attributionControl={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
+};
+
+export default MapInPost;
