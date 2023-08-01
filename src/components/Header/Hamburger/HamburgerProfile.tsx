7a32d277bf1e40ef9440040c2dc13335aac508bf
bloombar <amos@cs.nyu.edu>
2023-08-01 11:54:22 -0400

    working now

--- src/components/Header/Hamburger/HamburgerProfile.tsx
+++ src/components/Header/Hamburger/HamburgerProfile.tsx
@@ -2,12 +2,13 @@
 
 import UserPic from "../../Profile/UserPic";
 import UserInfo from "../../Profile/UserInfo";
+import userDefault from "../../../../public/media/user-default.svg";
 
 export default function Profile() {
   return (
     <section className="flex w-full justify-between items-center">
       <div className="w-9 h-9">
-        <UserPic />
+        <UserPic userPic={userDefault} />
       </div>
       <UserInfo />
     </section>
