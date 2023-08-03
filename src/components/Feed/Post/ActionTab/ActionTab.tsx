abf19d8febab8a0aaebdf371df72f5ca2aa19921
rubykiim <sk7383@nyu.edu>
2023-08-03 12:16:25 -0500

    adjust gap, set z-index

--- src/components/Feed/Post/ActionTab/ActionTab.tsx
+++ src/components/Feed/Post/ActionTab/ActionTab.tsx
@@ -6,8 +6,8 @@ type ActionTabProps = {};
 export const ActionTab: React.FC<ActionTabProps> = (props) => {
   return (
     <div
-      className="flex flex-col w-full justify-center items-center bg-white gap-4 pb-4
-      sticky top-60 sm:top-0"
+      className="flex flex-col w-full justify-center items-center bg-white
+      sticky top-60 sm:top-0 z-10"
     >
       <Dropdown />
       <ToolBar />
