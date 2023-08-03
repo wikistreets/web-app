9d7a2f7c5210b689d11d1f40287cba5769b6cfd7
rubykiim <sk7383@nyu.edu>
2023-08-03 12:15:29 -0500

    create Posts (with PostInfo)

---
+++ src/components/Feed/Post/Posts.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useContext } from "react";
+import { StaticImageData } from "next/image";
+import Post from "./Post";
+import PostInfo from "./PostInfo";
+import { SearchContext } from "@/context/SearchContext";
+
+type UserObject = {
+  userID: string;
+  userName: string | "";
+  userPic: StaticImageData;
+  maps: {
+    mapID: string;
+    mapType: string;
+    mapTitle: string;
+    mapData: {
+      centerX: number;
+      centerY: number;
+      markerX: number;
+      markerY: number;
+    };
+    posts: {
+      postID: string;
+      postTitle: string;
+      posted: number;
+      postMedia: StaticImageData[] | [];
+      location?: string;
+      caption?: string | null;
+      comments?: Array<{ commentText: string }> | [];
+    }[];
+  }[];
+};
+
+type PostsProps = {
+  users: UserObject[];
+};
+
+export const Posts: React.FC<PostsProps> = ({ users }) => {
+  const { searchIsClicked } = useContext(SearchContext);
+
+  // TODO: which posts to show & order
+  const Posts = users.map((user, idx) => {
+    return (
+      <Post
+        key={idx}
+        userID={user.userID}
+        userName={user.userName}
+        userPic={user.userPic}
+        maps={user.maps}
+      />
+    );
+  });
+
+  return (
+    <div
+      className="flex flex-col w-full h-full bg-white 
+          overflow-auto overscroll-contain scroll-smooth
+          sm:h-screen"
+    >
+      {/* TODO: POSTS ALSO NEED TO SHOW UP WHEN CLICKED OUTSIDE OF THE SEARCH ICON! */}
+      {searchIsClicked ? null : (
+        <>
+          <PostInfo
+            totalPosts={30}
+            totalContributors={9}
+            totalDuplicates={25}
+          />
+          {Posts}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
