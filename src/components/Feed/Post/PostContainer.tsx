2f3034dc4c25672455f6990e4e478c55b7d0a6f0
rubykiim <sk7383@nyu.edu>
2023-08-03 12:15:51 -0500

    add SearchContext

--- src/components/Feed/Post/PostContainer.tsx
+++ src/components/Feed/Post/PostContainer.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import { StaticImageData } from "next/image";
 import ActionTab from "./ActionTab/ActionTab";
-import Post from "./Post";
-import Info from "./ActionTab/Info";
+import Posts from "./Posts";
+import SearchContextProvider from "@/context/SearchContext";
 
+// TODO: create UserContext
 type UserObject = {
   userID: string;
   userName: string | "";
@@ -36,35 +36,14 @@ type PostContianerProps = {
   users: UserObject[];
 };
 
-const PostContainer = (props: PostContianerProps) => {
-  const [searchIsClicked, setSearchIsClicked] = useState(false);
-  // REACT CONTEXT!
-
-  // TODO: which posts to show & order
-  const Posts = props.users.map((user, idx) => {
-    return (
-      <Post
-        key={idx}
-        userID={user.userID}
-        userName={user.userName}
-        userPic={user.userPic}
-        maps={user.maps}
-      />
-    );
-  });
-  //   console.log("posts in PostContainer", Posts);
+export const PostContainer: React.FC<PostContianerProps> = ({ users }) => {
   return (
-    <section className="flex flex-col items-center justify-center h-full container">
-      <ActionTab />
-      <div
-        className="flex flex-col w-full h-full bg-white 
-        overflow-auto overscroll-contain scroll-smooth
-        sm:h-screen"
-      >
-        <Info totalPosts={30} totalContributors={9} totalDuplicates={25} />
-        {Posts}
-      </div>
-    </section>
+    <SearchContextProvider>
+      <section className="relative flex flex-col items-center justify-start h-full container">
+        <ActionTab />
+        <Posts users={users} />
+      </section>
+    </SearchContextProvider>
   );
 };
 
