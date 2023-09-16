aa3e6348f68b8833bb2027dc8fa33b96aaf10b78
rubykiim <sk7383@nyu.edu>
2023-09-16 15:40:20 +0800

    Create Map Detail page

---
+++ src/app/map/[id]/page.tsx
@@ -0,0 +1,13 @@
+import GetInTouch from "@/components/GetInTouch/GetInTouch";
+import Header from "@/components/Header/Header";
+import MapFeedContainer from "@/components/MapFeedContainer/MapFeedContainer";
+
+export default function Map({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <Header />
+      <MapFeedContainer />
+      <GetInTouch />
+    </>
+  );
+}
