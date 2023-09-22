fd10fa6b1f6bd85771e8fec53de11f4ecb516554
rubykiim <sk7383@nyu.edu>
2023-09-22 19:33:28 +0800

    Create user profile map page

---
+++ src/app/users/[id]/maps/page.tsx
@@ -0,0 +1,11 @@
+import MapListMultiple from "@/components/Maps/MapListMultiple";
+
+type Props = {};
+
+export default function Users({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <MapListMultiple />
+    </>
+  );
+}
