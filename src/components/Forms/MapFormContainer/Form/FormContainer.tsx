e5772256951335afcce243ca905ec481b58c5833
rubykiim <sk7383@nyu.edu>
2023-10-23 13:57:52 +0800

    Create FormContainer.tsx

---
+++ src/components/Forms/MapFormContainer/Form/FormContainer.tsx
@@ -0,0 +1,15 @@
+import MapForm from "./Form";
+import Selector from "@/components/MapFeedContainer/Feed/Selector";
+
+const FormContainer = () => {
+  return (
+    <>
+      <div className="flex flex-col h-full">
+        <Selector />
+        <MapForm />
+      </div>
+    </>
+  );
+};
+
+export default FormContainer;
