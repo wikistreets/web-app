91de8f5519f8a11a15c33f3783c0d285271f2a2d
rubykiim <sk7383@nyu.edu>
2024-01-31 16:32:24 +0900

    pointer on hover

--- src/components/FeaturedMaps/MapCard.tsx
+++ src/components/FeaturedMaps/MapCard.tsx
@@ -65,7 +65,7 @@ const MapCard = ({ data }: Data) => {
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <GeoJSON key={data.publicId} data={data} />
         </MapContainer>
-        <figcaption className="w-full py-4 text-center text-sm">
+        <figcaption className="w-full py-4 text-center text-sm hover:cursor-pointer">
           {data.title}
         </figcaption>
       </figure>
